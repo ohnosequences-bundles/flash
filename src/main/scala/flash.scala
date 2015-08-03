290172554f7d896197d5fc60583468eb86dd2630
eparejatobes <eparejatobes@ohnosequences.com>
2015-08-03 21:31:08 +0200

    should work install, basic untested cmd api
    I will finish the minimal API for building FLASh commands ASAP

--- src/main/scala/flash.scala
+++ src/main/scala/flash.scala
@@ -11,24 +11,54 @@ abstract class Flash(val version: String) extends Bundle(compressinglibs) { flas
   val folder: String = s"FLASH-${flash.version}"
   val flashBin: String = "flash"
 
-  final def install: Results = {
+  lazy val getTarball = Seq(
+    "wget",
+    s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/flash/${flash.version}/${flash.tarball}"
+  )
 
-    Seq(
-      "wget",
-      s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/flash/${flash.version}/${flash.tarball}"
-    ) ->-
-    Seq(
-      "make",
-      "-C",
-      s"${workingDir}/${flash.folder}"
-    ) ->-
-    Seq(
-      "ln",
-      "-s",
-      s"${workingDir}/${flash.folder}/${flash.flashBin}",
-      s"/usr/bin/${flash.flashBin}"
-    ) ->-
-    success(s"${bundleName} is installed")
+  lazy val extractTarball = Seq(
+    "tar",
+    "-xvzf",
+    s"${flash.tarball}"
+  )
+
+  lazy val compile = Seq(
+    "make",
+    "-C",
+    s"${workingDir}/${flash.folder}"
+  )
+
+  lazy val linkBinaries = Seq(
+    "ln",
+    "-s",
+    s"${workingDir}/${flash.folder}/${flash.flashBin}",
+    s"/usr/bin/${flash.flashBin}"
+  )
+
+  final def install: Results =
+    getTarball ->- extractTarball ->- compile ->- linkBinaries ->- success(s"${bundleName} installed")
+}
+
+object flashAPI {
+
+  trait FlashOption { def toSeq: Seq[String] }
+
+  case class minOverlap(val length: Int)          extends FlashOption { def toSeq = Seq("--minOverlap", s"${length}") }
+  case class maxOverlap(val length: Int)          extends FlashOption { def toSeq = Seq("--maxOverlap", s"${length}") }
+  case class threads(val number: Int)             extends FlashOption { def toSeq = Seq("--threads", s"${number}") }
+  case class readLen(val length: Float)           extends FlashOption { def toSeq = Seq("--readLen", s"${length}") }
+  case class fragmentLen(val length: Float)       extends FlashOption { def toSeq = Seq("--fragment-len", s"${length}") }
+  case class fragmentLenStddev(val length: Float) extends FlashOption { def toSeq = Seq("--fragment-len-stddev", s"${length}") }
+  object toStdout                                 extends FlashOption { def toSeq: Seq[String] = Seq("--to-stdout") }
+  object compress                                 extends FlashOption { def toSeq: Seq[String] = Seq("--compress") }
+
+  trait FlashInputFile { def toSeq: Seq[String] }
+
+  case class PairedEndFiles(val r1: String, val r2: String) extends FlashInputFile { def toSeq: Seq[String] = Seq(r1, r2) }
+
+  case class flashCmd(val input: FlashInputFile, val options: List[FlashOption]) {
+
+    def toSeq: Seq[String] = Seq("flash") ++ (options map { x => x.toSeq }).flatten ++ input.toSeq
   }
 
 }
