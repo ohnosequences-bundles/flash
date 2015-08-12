21e502ff7cfc5becd00410074387cea816bb7eed
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-08-12 15:59:54 +0200

    code cleaning services

--- src/main/scala/flashAPI.scala
+++ src/main/scala/flashAPI.scala
@@ -80,31 +80,35 @@ object flashAPI {
 
   class DefaultFlashOption(toSeq: Seq[String]) extends FlashOption(flash :~: ∅, toSeq)
 
-  case class minOverlap(val length: Int)          extends DefaultFlashOption( Seq("--minOverlap", s"${length}") )
-  case class maxOverlap(val length: Int)          extends DefaultFlashOption( Seq("--maxOverlap", s"${length}") )
-  case class threads(val number: Int)             extends DefaultFlashOption( Seq("--threads", s"${number}") )
-  case class readLen(val length: Float)           extends DefaultFlashOption( Seq("--readLen", s"${length}") )
-  case class fragmentLen(val length: Float)       extends DefaultFlashOption( Seq("--fragment-len", s"${length}") )
-  case class fragmentLenStddev(val length: Float) extends DefaultFlashOption( Seq("--fragment-len-stddev", s"${length}") )
-  object compress                                 extends DefaultFlashOption( Seq("--compress") )
-  object toStdout                                 extends DefaultFlashOption( Seq("--to-stdout") )
+  case class minOverlap(val length: Int)
+  extends DefaultFlashOption( Seq("--minOverlap", s"${length}") )
+
+  case class maxOverlap(val length: Int)
+  extends DefaultFlashOption( Seq("--maxOverlap", s"${length}") )
+
+  case class threads(val number: Int)
+  extends DefaultFlashOption( Seq("--threads", s"${number}") )
+
+  case class readLen(val length: Float)
+  extends DefaultFlashOption( Seq("--readLen", s"${length}") )
+
+  case class fragmentLen(val length: Float)
+  extends DefaultFlashOption( Seq("--fragment-len", s"${length}") )
+
+  case class fragmentLenStddev(val length: Float)
+  extends DefaultFlashOption( Seq("--fragment-len-stddev", s"${length}") )
+
+  case object compress
+  extends DefaultFlashOption( Seq("--compress") )
+
+  case object toStdout
+  extends DefaultFlashOption( Seq("--to-stdout") )
 
   trait FlashInputFile { def toSeq: Seq[String] }
 
   case class PairedEndFiles(val r1: File, val r2: File) extends FlashInputFile {
     def toSeq: Seq[String] = Seq(r1.getCanonicalPath, r2.getCanonicalPath)
   }
-
-  sealed trait AnyFlashCommandArguments {
-
-    type Commands <: AnyTypeSet.Of[AnyFlashCommand]
-    val commands: Commands
-
-    val toSeq: Seq[String]
-  }
-
-  abstract class FlashCommandArguments[Cmmnds <: AnyTypeSet.Of[AnyFlashCommand]](val commands: Cmmnds)
-  extends AnyFlashCommandArguments { type Commands = Cmmnds }
 
   // FLASH requires input files and just that? nope, more complicated than that.
   /*
@@ -118,23 +122,4 @@ object flashAPI {
 
     The `out` suffix is configurable. So we should configure the output folder. In the return type we should have either an execution error _or_ the corresponding output files.
   */
-
-  import ohnosequences.cosas.ops.typeSets.{ CheckForAll, ToList }
-
-  case class FlashExec[
-    Cmd <: AnyFlashCommand,
-    Opts <: AnyTypeSet.Of[AnyFlashOption]
-  ](
-    val command: Cmd,
-    // TODO arguments
-    val options: Opts
-  )(implicit
-    val ev: CheckForAll[Opts, OptionFor[Cmd]],
-    val toListEv: ToListOf[Opts, AnyFlashOption]
-  )
-  {
-
-    def toSeq: Seq[String] =  Seq(command.name) ++
-                              ( (options.toListOf[AnyFlashOption]) flatMap { _.toSeq } )
-  }
 }
